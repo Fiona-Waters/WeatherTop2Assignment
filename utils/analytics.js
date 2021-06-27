d4d1aaea27162dbe443e49de65f93392a67fc60e
Glitch (weather-top-2-assignment) <none>
2021-06-27 15:33:09 +0000

    🍵👆 Checkpoint ./utils/analytics.js:32645453/394

--- utils/analytics.js
+++ utils/analytics.js
@@ -3,25 +3,14 @@
 const stationList = require('./station-store.json').stationList;
 
 const analytics = {
-  
+  fillWeatherCodes(){
 const weatherCodes = {
+  
+  
   code:
   weather:
   
-  
+}
   
 }
-};
-
-const dashboard = {
-  index(request, response) {
-    logger.info("dashboard rendering");
-    const viewData = {
-      title: "WeatherTop Dashboard",
-      stations: stationList,
-      weatherCodes: weatherCodes,
-    };
-    logger.info('about to render', stationList);
-    response.render("dashboard", viewData);
-  },
 };
