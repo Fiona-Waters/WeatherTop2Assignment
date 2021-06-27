1d83a985799a44b43d47fa2e34b10a42fb77149b
Glitch (weather-top-2-assignment) <none>
2021-06-27 15:44:17 +0000

    🐸🏮 Checkpoint ./utils/analytics.js:32645453/272 ./views/partials/weathercards.hbs:32645453/22

--- utils/analytics.js
+++ utils/analytics.js
@@ -1,16 +1,19 @@
 'use strict';
 
-const stationList = require('./station-store.json').stationList;
+//const stationList = require('./station-store.json').stationList;
 
 const analytics = {
-  fillWeatherCodes(){
+  fillWeatherCodes(request, response){
 const weatherCodes = {
-  
-  
-  code:
-  weather:
-  
-}
-  
+  100: "Clear",
+  200: "Partial clouds",
+  300: "Cloudy",
+  400: "Light Showers",
+  500: "Heavy Showers",
+  600: "Rain",
+  700: "Snow",
+  800: "Thunder"
+} 
+response.render("dashboard", weatherCodes);
 }
 };
