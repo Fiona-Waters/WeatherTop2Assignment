f6001d0e53d0d563446fda7398885916d712f3ca
Glitch (weather-top-2-assignment) <none>
2021-06-27 21:06:24 +0000

    👨‍❤️‍💋‍👨❄️ Checkpoint ./models/station-store.js:32645453/479

--- models/station-store.js
+++ models/station-store.js
@@ -4,24 +4,10 @@ const stationList = require('./station-store.json').stationList;
 
 
 const station = {
-  public String name;
-  public float lat;
-  public float lng;
-  public String weatherCondition;
-  public String weatherConditionIcon;
-  public float fahrenheit;
-  public int toBeaufort;
-  public String windCompass;
-  public float windChill;
-  public float minTemperature;
-  public float maxTemperature;
-  public float maxWindSpeed;
-  public float minWindSpeed;
-  public float maxPressure;
-  public float minPressure;
-  public String tempTrend;
-  public String windTrend;
-  public String pressureTrend;
+  name;
+  lat;
+  lng;
+
 }
 
 
