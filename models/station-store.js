2bbabf26fe9f0e0666e02093ccf422754a139902
Glitch (weather-top-2-assignment) <none>
2021-06-27 10:49:00 +0000

    🌔🏟 Checkpoint ./models/station-store.json:32645453/1700 ./models/station-store.js:32645453/790

--- models/station-store.js
+++ models/station-store.js
@@ -1,39 +1,6 @@
 'use strict';
-const logger = require("../utils/logger");
 
-const tramore = {
-  name: 'Tramore',
-  readings: [
-    {
-      code: 800,
-      temperature: 0.5,
-      windSpeed: 3.5,
-    },
-    {
-      code: 600,
-      temperature: 6.0,
-      windSpeed: 2.0,
-    },
-  ],
-};
-
-const dunmore = {
-  name: 'Dunmore',
-  readings: [
-    {
-      code: 700,
-      temperature: 8.0,
-      windSpeed: 1.0
-    },
-    {
-      code: 200,
-      temperature: 0.5,
-      windSpeed: 3.5,
-    },
-  ],
-};
-
-const stationList = [tramore, dunmore];
-
+const stationList = require('./station-store.json').stationList;
 
 module.exports = stationList;
+
