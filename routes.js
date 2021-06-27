e6a9c4f3b7512970f11528c547fc809913923137
Glitch (weather-top-2-assignment) <none>
2021-06-27 13:07:25 +0000

    💺🎏 Checkpoint ./views/partials/menu.hbs:32645453/26 ./routes.js:32645453/61 ./controllers/main.js:32645453/294 ./views/layouts/main.hbs:32645453/53 ./views/home.hbs:32645453/2583 ./controllers/home.js:32645453/294

--- routes.js
+++ routes.js
@@ -5,10 +5,10 @@ const router = express.Router();
 
 const dashboard = require("./controllers/dashboard.js");
 const about = require("./controllers/about.js");
-const main = require("./controllers/main.js");
+const home = require("./controllers/home.js");
 
 router.get("/", dashboard.index);
 router.get("/dashboard", dashboard.index);
 router.get("/about", about.index);
-router.get("./layouts/main", main.index);
+router.get("/home", home.index);
 module.exports = router;
