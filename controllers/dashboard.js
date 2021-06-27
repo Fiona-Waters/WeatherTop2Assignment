eeb2c7242999745d653eb6d0a98afa4950ccb485
Glitch (weather-top-2-assignment) <none>
2021-06-27 08:57:18 +0000

    🎐🚶 Checkpoint ./views/partials/listreadings.hbs:32645453/665 ./controllers/dashboard.js:32645453/380

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,11 +2,28 @@
 
 const logger = require("../utils/logger");
 
+const stations = {
+  station: 'Tramore',
+  readings: [
+    {
+      code: 800,
+      temperature: 0.5,
+      windSpeed: 3.5,
+    },
+    {
+      code: 600,
+      temperature: 6.0,
+      windSpeed: 2.0,
+    },
+  ],
+};
+
 const dashboard = {
   index(request, response) {
     logger.info("dashboard rendering");
     const viewData = {
       title: "WeatherTop Dashboard",
+      station: stations
     };
     response.render("dashboard", viewData);
   },
