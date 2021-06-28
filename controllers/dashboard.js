5cc35e6fdf74cbae0a53e040a9dfccf0c1af66e2
Glitch (weather-top-2-assignment) <none>
2021-06-28 19:13:21 +0000

    🐥🌴 Checkpoint ./controllers/dashboard.js:32645453/385

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -4,7 +4,8 @@ const logger = require("../utils/logger");
 const stationList = require("../models/station-store.js");
 
 const setCardsForStation = function(station) {
-  logger.info("CardsFor", station);
+  station.name = station.name + " Ciaran is Cool";
+  return station;
 }
 
 
@@ -12,13 +13,15 @@ const dashboard = {
   index(request, response) {
     logger.info("dashboard rendering");
     
-    
-    
+    const newStationList = stationList.map(setCardsForStation);
+    /*
     for(let i = 0; i < stationList.length; i++) {
       const station = stationList[i];
       logger.info('Station', station);
+      
+      const lastReading = station.readings.length-1;
     }
-    
+    */
     
     
     
@@ -26,9 +29,9 @@ const dashboard = {
     
     const viewData = {
       title: "WeatherTop Dashboard",
-      stations: stationList
+      stations: newStationList
     };
-    logger.info('about to render', stationList);
+    //logger.info('about to render', stationList);
     response.render("dashboard", viewData);
   },
 };
