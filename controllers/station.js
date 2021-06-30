ae9aaaee8512d02bf8dd5de996094b15632b222a
Glitch (weather-top-2-assignment) <none>
2021-06-30 20:26:26 +0000

    🖖💡 Checkpoint ./views/station.hbs:32645453/36 ./views/partials/addstation.hbs:32645453/918 ./views/dashboard.hbs:32645453/23 ./routes.js:32645453/92 ./controllers/dashboard.js:32645453/1345 ./controllers/station.js:32645453/977 ./views/partials/weathercards.hbs:32645453/8

--- controllers/station.js
+++ controllers/station.js
@@ -3,14 +3,34 @@
 const uuid = require('uuid');
 const logger = require('../utils/logger');
 const stationStore = require('../models/station-store.js');
+const analytics = require('../utils/analytics.js');
 
 const station = {
   index(request, response) {
     const stationId = request.params.id;
     logger.debug("Station id = " + stationId);
+    const station = stationStore.getStation(stationId);
+    
+    if(station.readings.length>0){
+        
+      const lastReading = station.readings[station.readings.length-1];
+      station.lastReading = lastReading;
+           
+      let celsius = lastReading.temperature;
+      station.fahrenheit = analytics.convertCToF(celsius);
+      let windSpeed = lastReading.windSpeed;
+      station.beaufort = analytics.convertToBeaufort(windSpeed);
+      let code = lastReading.code;
+      station.weatherCondition = analytics.fillWeatherCodes(code);
+      let windDirection = lastReading.windDirection;
+      station.windCompass = analytics.calcWindDirection(windDirection);
+      station.windChill = analytics.calcWindChill(lastReading.temperature,lastReading.windSpeed);
+        
+      }
+    
     const viewData = {
       title: 'Station',
-      station: stationStore.getStation(stationId),
+      station: station,
     };
     response.render('station', viewData);
   },
